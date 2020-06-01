3059b00f70a62c2294c4438f649d3c5b13617f51
Yash <yash@khemani.com>
2020-06-01 17:29:24 +0000

    define variables for several paramters

--- main.tf
+++ main.tf
@@ -1,82 +1,101 @@
-provider "aws" {
-  region  = "us-east-1"
-  profile = "profile_name"
+provider aws {
+  region                 = var.aws_region
+  profile                = var.aws_profile # alternatively, set AWS_PROFILE environment variable.
   assume_role {
-    role_arn = "arn:aws:iam::00000000:role/role_name"
+    role_arn             = var.aws_auth_assume_role_arn
   }
 }
 
-
-data "aws_iam_role" "my-role" {
-  name = "role_to_assume"
+data aws_iam_role my-role {
+  name                   = var.instance_role
 }
 
-
-variable "subnet_prv1" {
-  description = "Private Subnet 1"
-  default = "subnet-xxxxxxxxxxxxxxxxx"
+data aws_security_group existing {
+  name                   = var.existing_security_group_name
 }
 
-
-resource "aws_security_group" "allow_net_ports" {
-  name        = "allow_net_ports"
-  description = "Allow inbound traffic"
-  vpc_id      = "vpc-id-to-use"
+resource aws_security_group allow_net_ports {
+  name                   = "allow_net_ports"
+  description            = "Allow inbound traffic"
+  vpc_id                 = var.vpc_id
 
   ingress {
-    description = "Web Node"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["10.128.0.0/16", "10.28.0.0/16"]
+    description          = "Allow http to port 8080 from _____"
+    from_port            = 8080
+    to_port              = 8080
+    protocol             = "tcp"
+    cidr_blocks          = ["10.128.0.0/16", "10.28.0.0/16"]
   }
 
   ingress {
-    description = "SSH Access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.128.0.0/16", "10.28.0.0/16"]
+    description          = "Allow ssh from _____"
+    from_port            = 22
+    to_port              = 22
+    protocol             = "tcp"
+    cidr_blocks          = ["10.128.0.0/16", "10.28.0.0/16"]
   }
-
 
   tags = {
-    Name = "allow_sg_rules"
+    Name                 = var.allow_net_ports_security_group_name
+  }
+}
+
+resource aws_iam_instance_profile my-profile {
+  name                   = var.instance_name
+  role                   = data.aws_iam_role.my-role.id
+}
+
+################################################################################
+# let's find the latest image for this owner
+data aws_ami ami {
+  most_recent            = true
+
+  filter {
+    name = "name"
+    values = ["hashistack-encrypted-*"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "tag:Owner"
+    values = [var.tag_owner]
+  }
+
+  owners = ["self"]
 }
 
-
-resource "aws_iam_instance_profile" "my-profile" {
-  name = var.instance_name
-  role = data.aws_iam_role.my-role.id
+# use the ami_id if it isn't an empty string. otherwise, use the hashistack image.
+locals {
+  ami_id = var.ami_id == "" ? data.aws_ami.ami.id : var.ami_id
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami         = "ami-xxxxxxxxxxxxx"
-  instance_type = "t2.medium"
-  key_name = "ssh_key"
-  iam_instance_profile = aws_iam_instance_profile.my-profile.id
-  subnet_id = var.subnet_prv1
-  
+resource aws_instance ec2_instance {
+  ami                    = var.ami_id
+  instance_type          = var.instance_size
+  key_name               = var.ssh_key
+  iam_instance_profile   = aws_iam_instance_profile.my-profile.id
+  subnet_id              = var.subnet_id
+
   root_block_device {
-    volume_type = "gp2"
-    encrypted = true
-    volume_size = 150
-    kms_key_id = "arn:aws:kms:us-east-1:000000000:key/key-id"
+    volume_type          = var.root_volume_type
+    encrypted            = var.root_volume_encrypt
+    volume_size          = var.root_volume_size
+    kms_key_id           = var.kms_key_id
   }
 
-  security_groups = [ "sg-existing-id", aws_security_group.allow_net_ports.id ]
- 
-  user_data = file("install_app.sh")
+  security_groups        = [ data.aws_security_group.existing.id, aws_security_group.allow_net_ports.id ]
+
+  user_data              = file(var.user_data_file)
 
   tags = {
-  Name = var.instance_name
-  Owner = "prash@email.com"
-  fm_aws_applicationId = "appid"
-  fm_aws_businessUnit = "it"
-  fm_aws_dCatalogue = "datacatolog-id"
+    Name                 = var.instance_name
+    Owner                = var.tag_owner
+    fm_aws_applicationId = var.tag_appid
+    fm_aws_businessUnit  = var.tag_bu
+    fm_aws_dCatalogue    = var.tag_dcid
   }
-
 }
-
--- variables.tf
+++ variables.tf
@@ -1,5 +1,123 @@
-variable "instance_name" {
+variable aws_region {
   type        = string
-  description = "Name of instance to provision."
+  description = "AWS region."
+  default     = "us-east-1"
 }
 
+variable aws_profile {
+  type        = string
+  description = "AWS profile."
+}
+
+variable aws_auth_assume_role_arn {
+  type        = string
+  description = "Role ARN for role to assume."
+}
+
+variable vpc_id {
+  type        = string
+  description = "VPC ID"
+}
+
+variable subnet_id {
+  type        = string
+  description = "Subnet ID for private subnet 1"
+}
+
+variable existing_security_group_name {
+  type        = string
+  description = "Name of existing security group to apply to instance"
+}
+
+variable allow_net_ports_security_group_name {
+  type        = string
+  description = "Name of allow net ports security group to create"
+}
+
+variable instance_role {
+  type        = string
+  description = "Name of AWS role for instance."
+}
+
+variable ami_id {
+  type        = string
+  description = "AMI to use for instance"
+  default     = ""
+}
+
+variable ami_filter {
+  type        = string
+  description = "Use existing AMI with this name filter"
+  default     = "hashistack-encrypted-*"
+}
+
+
+variable instance_size {
+  type        = string
+  description = "VPC ID"
+  default     = "t2.medium"
+}
+
+variable user_data_file {
+  type        = string
+  description = "Name of script to use for user data"
+  default     = "install_app.sh"
+}
+
+variable kms_key_id {
+  type        = string
+  description = "Existing KMS Key ID for encrypting root volume."
+  # default   = "arn:aws:kms:us-east-1:000000000:key/key-id"
+}
+
+variable root_volume_type {
+  type        = string
+  description = "Root volume type."
+  default     = "gp2"
+}
+
+variable root_volume_encrypt {
+  type        = bool
+  description = "Encrypt root volume?"
+  default     = true
+}
+
+variable root_volume_size {
+  type        = number
+  description = "Root volume size in GB."
+  default     = 150
+}
+
+variable instance_name {
+  type        = string
+  description = "Value for instance Name tag."
+}
+
+variable ssh_key {
+  type        = string
+  description = "SSH key name for logging into instance."
+}
+
+variable tag_owner {
+  type        = string
+  description = "Value for Owner tag"
+  #default    = "prash@email.com"
+}
+
+variable tag_appid {
+  type        = string
+  description = "Value for fm_aws_applicationId tag"
+  #default    = "appid"
+}
+
+variable tag_bu {
+  type        = string
+  description = "Value for fm_aws_businessUnit tag"
+  default     = "it"
+}
+
+variable tag_dcid {
+  type        = string
+  description = "Value for fm_aws_dCatalogue tag"
+  #default    = "datacatolog-id"
+}
